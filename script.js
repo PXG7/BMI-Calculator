ed45e211ccb3a57ac97a5b1ccde66fda4ec86599
Pokhrelgokul <pokhrelgokul2@gmail.com>
2026-06-29 19:11:55 +0545

    fix bmi calculator

--- script.js
+++ script.js
@@ -18,21 +18,26 @@ btn.addEventListener('click', function(){
 
     BMI = BMI.toFixed(2);
  
-    document.querySelector('#result').innerHTML = BMI;
-
+    const result = document.querySelector("#result");
+    result.innerHTML = BMI;
+    
     let status = "";
 
      if(BMI < 18.5) {
         status = "Underweight"
+        result.style.color = "#3498db"; // Blue
     }
     if(BMI >=18.5 && BMI < 25) {
         status = "Healthy";
+         result.style.color ="#2ecc71"; // Green
     }
      if(BMI >= 25 && BMI < 30) {
         status = "Overweight";
+         result.style.color = "#f39c12"; // Orange
     }
     if (BMI >= 30) {
         status = "Obese";
+         result.style.color = "#e74c3c"; // Red
     }
     document.querySelector(".comment").innerHTML = `Comment: you are <span id="comment">${status}</span> `;
 });
